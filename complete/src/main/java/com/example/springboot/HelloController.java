81eaab3447c6539ba490f78e5835e3a2ebbb1371
green-redcloud3 <62781092+green-redcloud3@users.noreply.github.com>
2020-05-01 09:27:27 -0400

    Testing commit

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! This is A Test Again Again";
+		return "Greetings from Spring Boot! This is A Test Again";
 	}
 
 }
