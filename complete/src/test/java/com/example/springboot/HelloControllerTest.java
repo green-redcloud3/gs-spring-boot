ed824cad71682995cafd4e05fc1d4e1d4d2d9b66
Muthu Vellayan <muthu.vellayan@gmail.com>
2020-05-27 15:12:09 -0400

    jenkins test

--- complete/src/test/java/com/example/springboot/HelloControllerTest.java
+++ complete/src/test/java/com/example/springboot/HelloControllerTest.java
@@ -24,6 +24,6 @@ public class HelloControllerTest {
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Greetings from Spring Boot! <b>Testing at 9:33 AM EST</b>")));
+				.andExpect(content().string(equalTo("Greetings from Spring Boot!"));
 	}
 }
