ca8f7a2dc8a26b656301a54c749c97f713c19424
Muthu Vellayan <muthu.vellayan@gmail.com>
2020-05-27 09:50:42 -0400

    jenkins test 2

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! Testing at 9:33 AM EST ";
+		return "Greetings from Spring Boot! Testing at 9:33 AM EST";
 	}
 
 }
